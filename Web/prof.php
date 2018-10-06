c40441d478ee3ae2aa0ead026318e5992d588e00
neelgeek <neelbhave91@gmail.com>
2018-10-06 23:30:27 +0530

    Integration with Classifier done

--- Web/prof.php
+++ Web/prof.php
@@ -32,7 +32,7 @@ $prof_info  = $prof->data();
 
  body{
      font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-     background-color: #2c2ea3;
+     background-color: #222222 ;
  }
  
 .topnav {
